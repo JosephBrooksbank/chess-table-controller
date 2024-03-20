859aadacbec344a32c344d162b06bb92ee59b5d0
Joseph Brooksbank <Joseph.Brooksbank@du.edu>
2024-03-19 22:06:56 -0500

    gpio start

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
-fn main() {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_svc::sys::link_patches();
+use esp_idf_hal::delay::FreeRtos;
+use esp_idf_hal::gpio::{self, PinDriver};
+use esp_idf_hal::peripherals::Peripherals;
+use esp_idf_hal::task::block_on;
 
-    // Bind the log crate to the ESP Logging facilities
+fn main() -> anyhow::Result<()> {
+    esp_idf_hal::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
+    // take control of peripherals
+    let peripherals = Peripherals::take()?;
 
-    log::info!("Hello, world!");
+    let mut step = PinDriver::output(peripherals.pins.gpio27)?;
+    let mut led = PinDriver::output(peripherals.pins.gpio2)?;
+    let mut button = PinDriver::input(peripherals.pins.gpio26)?;
+    button.set_pull(gpio::Pull::Up)?;
+
+    block_on(async {
+        loop {
+            button.wait_for_low().await.unwrap();
+            led.set_high().unwrap();
+
+            button.wait_for_high().await.unwrap();
+            led.set_low().unwrap();
+        }
+    })
 }
