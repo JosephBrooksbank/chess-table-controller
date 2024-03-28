ac477b8388bf86ee723c557ccaf8f2f6083def1a
Joseph Brooksbank <joseph.brooksbank@navex.com>
2024-03-28 08:19:38 -0700

    add stepper struct, try out async!!!
    interrupts will probably be the way in the long run, still exciting to see it working tho

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,56 @@
-use esp_idf_hal::delay::FreeRtos;
-use esp_idf_hal::gpio::{self, PinDriver};
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
+
+use esp_idf_hal::gpio::{self, Output, OutputPin, Pin, PinDriver};
+use esp_idf_hal::peripheral::Peripheral;
 use esp_idf_hal::peripherals::Peripherals;
-use esp_idf_hal::task::block_on;
+use esp_idf_sys::EspError;
+
+enum StepperDirection {
+    Clockwise,
+    Counterclockwise,
+}
+struct StepperMotor<'d, PinA, PinB>
+where
+    PinA: Pin,
+    PinB: Pin,
+{
+    step: PinDriver<'d, PinA, Output>,
+    dir: PinDriver<'d, PinB, Output>,
+}
+
+impl<'d, PinA, PinB> StepperMotor<'d, PinA, PinB>
+where
+    PinA: OutputPin,
+    PinB: OutputPin,
+{
+    fn new(
+        stepper_pin: impl Peripheral<P = PinA> + 'd,
+        dir_pin: impl Peripheral<P = PinB> + 'd,
+    ) -> Self {
+        StepperMotor {
+            step: PinDriver::output(stepper_pin).unwrap(),
+            dir: PinDriver::output(dir_pin).unwrap(),
+        }
+    }
+
+    fn step(&mut self) -> anyhow::Result<(), EspError> {
+        self.step.set_high()?;
+        std::thread::sleep(Duration::from_millis(5));
+        self.step.set_low()?;
+        std::thread::sleep(Duration::from_millis(5));
+        Ok(())
+    }
+
+    // todo! ensure these directions are correct.
+    fn set_direction(&mut self, dir: StepperDirection) -> anyhow::Result<(), EspError> {
+        match dir {
+            StepperDirection::Clockwise => self.dir.set_high(),
+            StepperDirection::Counterclockwise => self.dir.set_low(),
+        }
+    }
+}
 
 fn main() -> anyhow::Result<()> {
     esp_idf_hal::sys::link_patches();
@@ -9,18 +58,40 @@ fn main() -> anyhow::Result<()> {
     // take control of peripherals
     let peripherals = Peripherals::take()?;
 
-    let mut step = PinDriver::output(peripherals.pins.gpio27)?;
+    let mut stepper = StepperMotor::new(peripherals.pins.gpio27, peripherals.pins.gpio14);
+    stepper.set_direction(StepperDirection::Clockwise)?;
     let mut led = PinDriver::output(peripherals.pins.gpio2)?;
     let mut button = PinDriver::input(peripherals.pins.gpio26)?;
-    button.set_pull(gpio::Pull::Up)?;
+    button.set_pull(gpio::Pull::Down)?;
 
-    block_on(async {
-        loop {
-            button.wait_for_low().await.unwrap();
-            led.set_high().unwrap();
+    let m = Arc::new(Mutex::new(true));
 
-            button.wait_for_high().await.unwrap();
-            led.set_low().unwrap();
+    let m2 = m.clone();
+    thread::spawn(move || loop {
+        let on;
+        {
+            let mutex_ref = m2.lock().unwrap();
+            on = *mutex_ref;
         }
-    })
+        led.set_level(match on {
+            true => gpio::Level::High,
+            false => gpio::Level::Low,
+        })
+        .unwrap();
+
+        thread::sleep(Duration::from_millis(10));
+    });
+
+    loop {
+        if button.is_high() {
+            let mut mutex_ref = m.lock().unwrap();
+            *mutex_ref = true;
+            println!("Sending Pulse to Stepper!");
+        } else {
+            let mut mutex_ref = m.lock().unwrap();
+            *mutex_ref = false;
+        }
+
+        thread::sleep(Duration::from_millis(10));
+    }
 }
