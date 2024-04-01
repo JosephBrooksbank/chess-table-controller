352310f70f276360163f97762367fce34bf8f578
Joseph Brooksbank <Joseph.Brooksbank@du.edu>
2024-04-01 18:07:22 -0500

    change to single thread, single notification
    I don't know why I didn't consider using the same notifier in multiple interrupts before. Simplifies things greatly.

--- src/gpio/stepper.rs
+++ src/gpio/stepper.rs
@@ -1,13 +1,12 @@
 use std::num::NonZeroU32;
-use std::sync::{Arc, mpsc, Mutex};
-use std::thread;
+
+use esp_idf_hal::{delay, timer};
 use esp_idf_hal::delay::Ets;
 use esp_idf_hal::gpio::{Output, OutputPin, Pin, PinDriver};
 use esp_idf_hal::peripheral::Peripheral;
 use esp_idf_hal::task::notification::Notification;
-use esp_idf_hal::timer;
-use esp_idf_hal::timer::{TimerConfig, TimerDriver};
-use esp_idf_sys::{EspError, qsort};
+use esp_idf_hal::timer::{Timer, TimerConfig, TimerDriver};
+use esp_idf_sys::EspError;
 use log::info;
 use serde::Deserialize;
 
@@ -23,39 +22,39 @@ enum StepperState {
     Accelerating,
     Constant,
     Decelerating,
-    CurrentStep(u32),
 }
 
-pub struct StepperMotor< PinA, PinB>
-where
-    PinA: Pin,
-    PinB: Pin,
+pub trait Stepper {
+    fn step(&mut self, pulse_width: u32) -> anyhow::Result<(), EspError>;
+    fn set_direction(&mut self, dir: StepperDirection) -> anyhow::Result<(), EspError>;
+    fn drive(&mut self, steps: u32, pulse_width: u32, accel: u64, max_sps: u64) -> anyhow::Result<(), EspError>;
+}
+
+pub struct StepperMotor<PinA, PinB, TIMER00, TIMER01>
+    where
+        PinA: Pin,
+        PinB: Pin,
 {
     step: PinDriver<'static, PinA, Output>,
     dir: PinDriver<'static, PinB, Output>,
-    sps_timer: Arc<Mutex<TimerDriver<'static >>>,
-    accel_timer: Arc<Mutex<TimerDriver<'static >>>,
+    sps_timer: TIMER00,
+    accel_timer: TIMER01,
 
 }
 
-impl<PinA, PinB> StepperMotor<PinA, PinB>
-where
-    PinA: OutputPin,
-    PinB: OutputPin,
+impl<PinA, PinB, TIMER00, TIMER01> StepperMotor<PinA, PinB, TIMER00, TIMER01>
+    where
+        PinA: OutputPin,
+        PinB: OutputPin,
+        TIMER00: Peripheral<P=timer::TIMER00>,
+        TIMER01: Peripheral<P=timer::TIMER01>,
 {
     pub fn new(
-        stepper_pin: impl Peripheral<P = PinA> + 'static,
-        dir_pin: impl Peripheral<P = PinB> + 'static,
-        sps_timer: impl Peripheral<P = timer::TIMER00> + 'static,
-        accel_timer: impl Peripheral<P = timer::TIMER01> + 'static
+        stepper_pin: impl Peripheral<P=PinA> + 'static,
+        dir_pin: impl Peripheral<P=PinB> + 'static,
+        sps_timer: TIMER00,
+        accel_timer: TIMER01,
     ) -> Self {
-
-        let mut timer_config = TimerConfig::new();
-        timer_config.auto_reload = true;
-        timer_config.divider = 10;
-        let sps_timer = Arc::new(Mutex::new(TimerDriver::new(sps_timer, &timer_config).unwrap()));
-        let accel_timer = Arc::new(Mutex::new(TimerDriver::new(accel_timer, &timer_config).unwrap()));
-
         let mut stepper = StepperMotor {
             step: PinDriver::output(stepper_pin).unwrap(),
             dir: PinDriver::output(dir_pin).unwrap(),
@@ -81,173 +80,117 @@ where
             StepperDirection::Counterclockwise => self.dir.set_low(),
         }
     }
-    
+
     // const MAX_SPS: u64 = 1000;
     // const ACCEL: u64 = 100;
     const GRANULARITY: u64 = 1;
-    
+
     const ACCEL_PERCENT: f64 = 0.2;
 
+    fn setup_timers<'d, Timer00, Timer1>(timer00: impl Peripheral<P=Timer00> + 'd, timer01: impl Peripheral<P=Timer1> + 'd) -> (TimerDriver<'d>, TimerDriver<'d>)
+        where
+            Timer00: Timer,
+            Timer1: Timer
+    {
+        let mut timer_config = TimerConfig::new();
+        timer_config.auto_reload = true;
+        timer_config.divider = 10;
+
+        let sps_timer = TimerDriver::new(timer00, &timer_config).unwrap();
+        let accel_timer = TimerDriver::new(timer01, &timer_config).unwrap();
+        (sps_timer, accel_timer)
+    }
 
     pub unsafe fn drive(&mut self, steps: u32, pulse_width: u32, accel: u64, max_sps: u64) -> anyhow::Result<(), EspError> {
-        let mut counter: u32 = 0;
-        let mut sps = 1;
-        let accels_per_sec =  accel / Self::GRANULARITY;
-        let notification = Notification::new();
-        let notifier = notification.notifier();
-        let stop_accel = (steps as f64 *Self::ACCEL_PERCENT).floor() as u32;
+        let sps_timer = self.sps_timer.clone_unchecked();
+        let accel_timer = self.accel_timer.clone_unchecked();
+        let (mut sps_timer, mut accel_timer) = Self::setup_timers(sps_timer, accel_timer);
+        let mut current_sps = 1;
+        let mut current_step = 0;
+        
+        let stop_accel = (steps as f64 * Self::ACCEL_PERCENT).floor() as u32;
         let mut start_decel = steps - stop_accel;
         
-        // initial set up of timer in lower scope so the lock is released
-        {
-            let mut sps_timer = self.sps_timer.lock().unwrap();
-            let timer_hz = sps_timer.tick_hz();
-            let sps_timer_register = timer_hz / sps;
 
+        let instruction_notifications = Notification::new();
+        let sps_instruction_notifier = instruction_notifications.notifier();
+        let accel_instruction_notifier = instruction_notifications.notifier();
+        let timer_hz = sps_timer.tick_hz();
 
-            sps_timer.subscribe(move || {
-                let bitset = 0b10001010101;
-                notifier.notify_and_yield(NonZeroU32::new(1).unwrap());
-            })?;
-            sps_timer.set_alarm(sps_timer_register)?;
-            sps_timer.enable_interrupt()?;
-            sps_timer.enable_alarm(true)?;
-            sps_timer.enable(true)?;
-        }
-        
-        let sps_timer_ref = self.sps_timer.clone();
-        let accel_timer_ref = self.accel_timer.clone();
-        let (stepper_state_tx, stepper_state_rx) = mpsc::channel();
-        let (decel_tx, decel_rx) = mpsc::channel();
-        
-        
-        
-        thread::spawn(move || {
-            info!("starting acceleration thread");
-            let mut sps_timer = sps_timer_ref.lock().unwrap();
-            let mut accel_timer = accel_timer_ref.lock().unwrap();
-            let timer_hz = sps_timer.tick_hz();
-            let mut accel_timer_register = accel_timer.tick_hz() / accels_per_sec as u64;
-            info!("grabbed locks");
-            let notification = Notification::new();
-            let notifier = notification.notifier();
-            accel_timer.subscribe(move || {
-                let bitset = 0b10001010101;
-                notifier.notify_and_yield(NonZeroU32::new(1).unwrap());
-            }).unwrap();
-            accel_timer.set_alarm(accel_timer_register).unwrap();
-            accel_timer.enable_alarm(true).unwrap();
-            accel_timer.enable_interrupt().unwrap();
-            accel_timer.enable(true).unwrap();
-            let mut stepper_state = StepperState::Accelerating;
-            let mut current_step = 0;
-            loop {
-                if stepper_state == StepperState::Constant {
-                    match stepper_state_rx.recv() {
-                        Ok(val) => {
-                            match val {
-                                StepperState::Accelerating => {
-                                    accel_timer.enable_alarm(true).unwrap();
-                                    stepper_state = StepperState::Accelerating;
-                                }
-                                StepperState::Decelerating => {
-                                    accel_timer.enable_alarm(true).unwrap();
-                                    stepper_state = StepperState::Decelerating;
-                                }
-                                StepperState::Constant => {
-                                    continue;
-                                },
-                                StepperState::CurrentStep(step) => {
-                                    current_step = step;
-                                }
-                            }
-                        },
-                        Err(_) => {
-                            break;
-                        }
-                    }
-                }
-                match stepper_state_rx.try_recv() {
-                    Ok(val) => {
-                        match val {
-                            StepperState::CurrentStep(step) => {
-                                current_step = step;
-                            }
-                            _ => {
-                                stepper_state = val;
-                            }
-                        }
-                    }
-                    Err(e) => {
-                        if let mpsc::TryRecvError::Disconnected = e {
-                            break;
-                        }
-                    }
-                }
-                let bitset = notification.wait(esp_idf_hal::delay::BLOCK);
-                if let Some(_bitset) = bitset {
-                    match stepper_state {
-                        StepperState::Accelerating => sps += 1,
-                        StepperState::Constant => {
-                            accel_timer.enable_alarm(false).unwrap();
-                            continue;
-                        },
-                        StepperState::Decelerating => sps -= 1,
-                        StepperState::CurrentStep(step) => {
-                            current_step = step;
-                        }
-                    }
-                    if sps > max_sps {
-                        info!("setting the point to decelerate to {}", steps - current_step);
-                        decel_tx.send(current_step).unwrap();
-                        sps = max_sps; 
-                        stepper_state = StepperState::Constant;
-                    }
-                    if sps == 0 {
-                        sps = 1;
-                    }
-                    info!("sps: {}", sps);
-                    sps_timer.set_alarm(timer_hz / sps).unwrap();
-                    sps_timer.enable_alarm(true).unwrap()
-                }
-            }
-        });
+        let sps_timer_register = timer_hz / current_sps;
+        sps_timer.subscribe(move || {
+            sps_instruction_notifier.notify_and_yield(NonZeroU32::new(Instruction::Step as u32).unwrap());
+        }).unwrap();
+        sps_timer.set_alarm(sps_timer_register).unwrap();
+        sps_timer.enable_alarm(true).unwrap();
+        sps_timer.enable_interrupt().unwrap();
+        sps_timer.enable(true).unwrap();
 
+        let accel_timer_register = accel_timer.tick_hz() / accel;
+        accel_timer.set_alarm(accel_timer_register).unwrap();
+        accel_timer.enable_alarm(true).unwrap();
+        accel_timer.subscribe(move || {
+            accel_instruction_notifier.notify_and_yield(NonZeroU32::new(Instruction::Accelerate as u32).unwrap());
+        }).unwrap();
+        accel_timer.enable_interrupt().unwrap();
+        accel_timer.enable(true).unwrap();
+
+        
+        let mut stepper_state = StepperState::Accelerating;
 
         loop {
-            if counter == steps {
-                break;
-            }
-            match decel_rx.try_recv() {
-                Ok(step) => {
-                    info!("setting the point to decelerate to {}", steps - step);
-                    // start_decel = steps - step;
-                }
-                Err(e) => {
-                    if let mpsc::TryRecvError::Disconnected = e {
-                        break;
+            match instruction_notifications.wait(delay::BLOCK) {
+                None => {}
+                Some(instruction) => {
+                    match instruction.get() {
+                        1 => {
+                            self.step(pulse_width)?;
+                            current_step += 1;
+                            info!("current step: {}", current_step);
+                            if current_step >= steps {
+                                break;
+                            }
+                            
+                            if current_step == stop_accel {
+                                stepper_state = StepperState::Constant;
+                            }
+                            if current_step >= start_decel {
+                                stepper_state = StepperState::Decelerating;
+                            }
+                        }
+                        2 => {
+                            match stepper_state {
+                                StepperState::Accelerating => {
+                                    current_sps += 1;
+                                    if current_sps >= max_sps {
+                                        stepper_state = StepperState::Constant;
+                                        current_sps = max_sps;
+                                        start_decel = steps - current_step;
+                                    }
+                                }
+                                StepperState::Constant => {continue;}
+                                StepperState::Decelerating => {
+                                    current_sps -= 1;
+                                    if current_sps == 0 {
+                                        current_sps = 1;
+                                    }
+                                    stepper_state = StepperState::Constant;
+                                }
+                            }
+                            info!("current sps: {}", current_sps);
+                            let sps_timer_register = timer_hz / current_sps;
+                            sps_timer.set_alarm(sps_timer_register).unwrap();
+                        }
+                        _ => {}
                     }
                 }
             }
-                
-            let bitset = notification.wait(esp_idf_hal::delay::BLOCK);
-            if let Some(bitset) = bitset {
-                self.step(pulse_width)?;
-                counter += 1;
-                println!("on step: {}", counter);
-                if counter == stop_accel {
-                    println!("stop accel");
-                    stepper_state_tx.send(StepperState::Constant).unwrap();
-                }
-                else if counter == start_decel {
-                    println!("start decel");
-                    stepper_state_tx.send(StepperState::Decelerating).unwrap();
-                } else {
-                    stepper_state_tx.send(StepperState::CurrentStep(counter)).unwrap();
-                }
-            }
         }
-            
+
         Ok(())
     }
+}
+enum Instruction {
+    Step = 1,
+    Accelerate = 2,
 }
